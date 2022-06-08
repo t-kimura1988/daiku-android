ac358d372a83c24c7d54a46858dadcafad097096
t-kimura1988 <ptgrs.03@gmail.com>
2022-06-08 14:20:56 +0900

    画像追加

--- auth/src/main/java/com/goen/auth/presentation/SignInMenu.kt
+++ auth/src/main/java/com/goen/auth/presentation/SignInMenu.kt
@@ -4,7 +4,7 @@ import com.goen.auth.R
 
 sealed class SignInMenu(var title: String, var icon: Int) {
     object GoogleSignIn : SignInMenu("Googleアカウントでログイン", R.drawable.ic_btn_google_light_normal_ios)
-    object PrivacyPolicy : SignInMenu("プライバシーポリシー", R.drawable.ic_btn_google_light_normal_ios)
-    object TermsOfUse : SignInMenu("利用規約", R.drawable.ic_btn_google_light_normal_ios)
+    object PrivacyPolicy : SignInMenu("プライバシーポリシー", R.drawable.menu_list)
+    object TermsOfUse : SignInMenu("利用規約", R.drawable.menu_list)
 
 }
